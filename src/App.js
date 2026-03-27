79f89df42c81926b2f2e76e8c575710fd7682365
Quy Tran <quytran11204@gmail.com>
2026-03-27 18:23:05 +0700

    tạo điều hướng trang với link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "./components/Header/Header";
-
+import { Link } from "react-router";
 
 
 const App = () => {
@@ -8,7 +8,17 @@ const App = () => {
   return (
     <div className="App">
       <Header/>
-      
+      <div>
+          test link 
+          <div>
+            <button>
+            <Link to="/users"> go  to user page</Link>
+              </button>
+            <button>
+              <Link to="/admins"> go  to admin page</Link>
+            </button>
+          </div>    
+      </div>
     </div>
   )
 
