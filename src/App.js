d003419d3b9abf9c2bc12abff9239be2796ccd52
Quy Tran <quytran11204@gmail.com>
2026-03-27 17:39:46 +0700

    setup header đơn giản

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
+import Header from "./components/Header/Header";
 
+
+
+const App = () => {
+  
   return (
-    <div> 
-    hello world
+    <div className="App">
+      <Header/>
+      
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       hello world
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+  )
+
+    
 }
 
 export default App;
